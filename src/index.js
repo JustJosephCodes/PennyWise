4de7d62504421bc3f9a7f47de84627426c054775
Joseph Benamaisia <josephbenamaisia@gmail.com>
2024-08-08 22:15:36 +0100

    redesigned auth page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from "firebase/app";
-import { getFirestore, } from "firebase/firestore";
-
-initializeApp(firebaseConfig);
-const db = getFirestore();
+import { getFirestore, collection, onSnapshot, addDoc, deleteDoc, doc, query, where, orderBy, serverTimestamp, getDoc, updateDoc} from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDS3I7JronhlZIVeLzecxiG2mukkLjdY4",
@@ -14,3 +12,34 @@ const firebaseConfig = {
   appId: "1:731321605151:web:589b6cab9066458f90892a",
   measurementId: "G-JKYX0WKXFP",
 };
+
+//init firebase
+initializeApp(firebaseConfig)
+
+//init services
+const db = getFirestore();
+const auth = getAuth()
+
+//colref
+const colref = collection(db, `Log-in's`)
+
+// authentication 
+
+//signing users up
+const form = document.querySelector("#auth-form")
+form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  let email = document.querySelector("#email");
+  let password = document.querySelector("#password");
+  email = email.value 
+  password = password.value
+  
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred)=>{
+      console.log(`user is`,cred.user);
+      form.reset()
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
+} )
